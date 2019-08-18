7b45c3e0895b170dd365d3c9c1a594ce36b0aaa9
Jamey-Dogom <33010705+AppJedi@users.noreply.github.com>
2019-08-17 17:32:03 -0700

    initial commit of project setup

---
+++ src/main/java/com/jd/endfinite/EndfiniteApplication.java
@@ -0,0 +1,13 @@
+package com.jd.endfinite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndfiniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndfiniteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jd/endfinite/EndfiniteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jd.endfinite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EndfiniteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
