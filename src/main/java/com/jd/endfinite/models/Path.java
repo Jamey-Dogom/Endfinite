f89f2c47e47a3d691e91c8aa46a94e573d066b80
Jamey-Dogom <33010705+AppJedi@users.noreply.github.com>
2019-08-17 18:38:15 -0700

    created models for endfinite

---
+++ src/main/java/com/jd/endfinite/models/Path.java
@@ -0,0 +1,107 @@
+package com.jd.endfinite.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "paths")
+public class Path {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Size(min = 1, message = "Cannot be empty")
+	private String choice1;
+	@Size(min = 1, message = "Cannot be empty")
+	private String choice2;
+	@Size(min = 1, message = "Cannot be empty")
+	private String choice3;
+	@Column(updatable = false)
+	private Date createdAt;
+	private Date updatedAt;
+
+	// Many users can be the One author for a story
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "stage_id")
+	private Stage stage;
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getChoice1() {
+		return choice1;
+	}
+
+	public void setChoice1(String choice1) {
+		this.choice1 = choice1;
+	}
+
+	public String getChoice2() {
+		return choice2;
+	}
+
+	public void setChoice2(String choice2) {
+		this.choice2 = choice2;
+	}
+
+	public String getChoice3() {
+		return choice3;
+	}
+
+	public void setChoice3(String choice3) {
+		this.choice3 = choice3;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Stage getStage() {
+		return stage;
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+
+	// other getters and setters removed for brevity
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+
+}
